532f71da1fd699639b903915dbd1bccc490a76e5
369 <master369_@hotmail.com>
2021-09-03 01:29:31 +0800

    feat: add copy function

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const helper = require('./shortlink_generator')
 
 const app = express()
 const port = 3000
 
+mongoose.connect('mongodb://localhost/linkShortener', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb erro!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.engine('hbs', exhbs({ defultLayout: 'main', extname:'.hbs'}))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.post('/', (req, res) => {
+  const inputLink = req.body.URLlink
+  console.log(inputLink)
+  const shortenLink = helper.judgeLink(inputLink)
+  res.render('index', { shortenLink, inputLink })
+})
+
 
 app.listen(port, () => {
   console.log('App is running!')
---
+++ shortlink_generator.js
@@ -0,0 +1,33 @@
+function judgeLink(link) {
+  if ((link.length === 0) || (link.includes('https://') == ! true)) {
+    return {
+      error: true,
+      value:'Please input a valid link.'
+    }
+  } else {
+    return {
+      error: false,
+      value: shortLinkGenerator(link)
+    }
+  }
+}
+
+function shortLinkGenerator(link) {
+  const library = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+  const splitLibrary = library.split('')
+  let shortenLink = 'https://aga-url-shortener.herokuapp.com/'
+
+  for ( let i = 0; i < 5 ; i++) {
+    const index = Math.floor(Math.random() * splitLibrary.length)
+    shortenLink += splitLibrary[index]
+  }
+  return shortenLink
+} 
+
+const helper = {
+  shortLinkGenerator: shortLinkGenerator,
+  judgeLink: judgeLink
+
+}
+
+module.exports = helper
